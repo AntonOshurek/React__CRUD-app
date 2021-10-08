2c35f8ed4b870e04579d7a630ca19da68da63832
AntonOshurek <oshurekweb@gmail.com>
2021-10-08 10:19:57 +0200

    create new data base

--- crud-app/src/components/app/app.js
+++ crud-app/src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John C' , salary: 800},
+        {name: 'Alex M' , salary: 3000},
+        {name: 'Carl W' , salary: 5000}
+    ];
+
     return(
         <div className='app'>
             <AppInfo />
