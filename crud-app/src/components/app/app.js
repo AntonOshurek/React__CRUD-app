9a08c64688af08fc1a8be392fe88e08df4408c0c
AntonOshurek <oshurekweb@gmail.com>
2021-10-09 12:22:14 +0200

    rewrite app js for constructor

--- crud-app/src/components/app/app.js
+++ crud-app/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,36 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
 
-  const data = [
-    {name: 'John C', salary: 800, increase: false, id: 1},
-    {name: 'Alex M', salary: 3000, increase: true, id: 2},
-    {name: 'Carl W', salary: 5000, increase: false, id: 3},
-  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C', salary: 800, increase: false, id: 1},
+        {name: 'Alex M', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W', salary: 5000, increase: false, id: 3},
+      ]
+    }
+  }
 
-  return(
-    <div className='app'>
-      <AppInfo />
+  render() {
+    return(
+      <div className='app'>
+        <AppInfo />
 
-    <div className="search-panel">
-      <SearchPanel/>
-      <AppFilter/>
-    </div>
+      <div className="search-panel">
+        <SearchPanel/>
+        <AppFilter/>
+      </div>
 
-    <EmployersList data={data}/>
-    <EmployersAddForm/>
-    </div>
-  );
+      <EmployersList
+        data={this.state.data}
+        onDelete={id => console.log(id)}/>
+      <EmployersAddForm/>
+      </div>
+    );
+  }
 }
 
 export default App;
