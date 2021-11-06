c8bed68c2a08b28fe9dc60e0d81e07f9feacb30d
AntonOshurek <oshurekweb@gmail.com>
2021-11-06 23:25:27 +0100

    delete all logick

--- crud-app/src/components/app/app.js
+++ crud-app/src/components/app/app.js
@@ -10,46 +10,17 @@ import './app.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        {name: 'John C', salary: 800, increase: false, id: 1},
-        {name: 'Alex M', salary: 3000, increase: true, id: 2},
-        {name: 'Carl W', salary: 5000, increase: false, id: 3},
-      ]
-    }
-  }
-
-  deleteItem = (id) => {
-    this.setState(({data}) => {
-      //const index = data.findIndex(elem => elem.id === id);
-
-      //const before = data.slice(0, index);
-      //const after = data.slice(index + 1);
-
-      //const newArr = [...before, ...after]
-
-      return {
-        data: data.filter(item => item.id !== id)
-      }
-    })
-  }
-
   render() {
     return(
       <div className='app'>
         <AppInfo />
 
-      <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
-      </div>
-
-      <EmployersList
-        data={this.state.data}
-        onDelete={this.deleteItem}/>
-      <EmployersAddForm/>
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+        <EmployersList/>
+        <EmployersAddForm/>
       </div>
     );
   }
