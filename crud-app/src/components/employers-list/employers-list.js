21e03ee3d24ff7bedb3c001e0ca1c4c6a7b2d93f
AntonOshurek <oshurekweb@gmail.com>
2021-12-01 23:28:06 +0100

    add delete option

--- crud-app/src/components/employers-list/employers-list.js
+++ crud-app/src/components/employers-list/employers-list.js
@@ -2,12 +2,16 @@ import EmployersListItem from "../employers-list-item/employers-list-item";
 
 import './employers-list.css';
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
 
   const elements = data.map((item, i) => { //если с бэка не пришли id то можно поставить индекс
     const {id, ...itemProps} = item; //если с бэка пришли, то деструкруризовать и назначить в атрибут key
     return (
-      <EmployersListItem  key={id} {...itemProps}/> //name={item.name} salary={item.salary} <= тоже самое!!!
+      <EmployersListItem
+            key={id}
+            {...itemProps} //name={item.name} salary={item.salary} <= тоже самое!!!
+            onDelete={() => onDelete(id)}
+      />
     )
   });
 
